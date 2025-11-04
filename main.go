08923350418bd5a4b2a01e96838f587dca68fb17
Simon Tucker <brotherlogic@gmail.com>
2025-11-03 17:20:45 -0800

    Handle the ssh known hosts file

--- main.go
+++ main.go
@@ -7,10 +7,31 @@ import (
 	"github.com/brotherlogic/tasklister/server"
 )
 
+func writeString(filename, text string) error {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(text); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	log.Printf("Starting")
 	s := server.NewServer()
-	err := s.Test(os.Getenv("DEPLOY_KEY"))
+
+	// Setup known_hosts
+	err := writeString(os.Getenv("SSH_KNOWN_HOSTS"), os.Getenv("SSH_ENTRY"))
+	if err != nil {
+		log.Fatalf("Unable to write ssh entry: %v", err)
+	}
+
+	err = s.Test(os.Getenv("DEPLOY_KEY"))
 	if err != nil {
 		log.Fatalf("Error testing: % v -> %v", err, os.Getenv(("SSH_KNOWN_HOSTS")))
 	}
